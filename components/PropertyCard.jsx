f275f3dc6a734e29168bf39daa472266ab4fb3c3
Quroz <filipwt@hotmail.com>
2024-05-14 10:25:49 +0200

    property card

--- components/PropertyCard.jsx
+++ components/PropertyCard.jsx
@@ -1,10 +1,49 @@
-import { Pressable, StyleSheet, Text, View } from 'react-native'
+import { Dimensions, Image, Pressable, StyleSheet, Text, View } from 'react-native'
 import React from 'react'
+import { AntDesign, Entypo, Ionicons, Octicons, FontAwesome5, MaterialIcons } from "@expo/vector-icons"
 
-const PropertyCard = () => {
+const PropertyCard = ({ rooms, children, adults, selectedDates, property, availableRooms }) => {
+    const { width, height } = Dimensions.get("window")
     return (
         <View>
-            <Pressable />
+            <Pressable style={{ margin: 15, flexDirection: "row", backgroundColor: "white" }}>
+                <View>
+                    <Image source={{ uri: property.image }} style={{ width: width - 280, height: height / 4 }} />
+                </View>
+
+                <View style={{ padding: 10 }}>
+                    <View style={{ flexDirection: "row", alignItems: "center", justifyContent: "space-between" }}>
+                        <Text style={{ width: 200 }}>{property.name}</Text>
+                        <AntDesign name="hearto" size={24} color="red" />
+                    </View>
+
+                    <View style={{ flexDirection: "row", alignItems: "center", gap: 6, marginTop: 5 }}>
+                        <MaterialIcons name="stars" size={24} color="green" />
+                        <Text>{property.rating}</Text>
+                        <View style={{ backgroundColor: "#6cb4ee", paddingVertical: 3, borderRadius: 5, width: 100 }}>
+                            <Text style={{ textAlign: "center", color: "white", fontSize: 15 }}>Genious Level</Text>
+                        </View>
+                    </View>
+
+                    <Text style={{ width: 210, marginTop: 6, color: "gray", fontWeight: "bold" }}>{property.address.length > 50 ? property.address.substring(0, 50) : property.address}</Text>
+
+                    <Text style={{ marginTop: 4, fontSize: 15, fontWeight: "500" }}>Price for 1 Night and {adults} adults</Text>
+
+                    <View style={{ marginTop: 5, flexDirection: "row", alignItems: "center", gap: 8 }}>
+                        <Text style={{ color: "red", fontSize: 18, textDecorationLine: "line-through" }}>${property.oldPrice * adults}</Text>
+                        <Text style={{ color: "black", fontSize: 18, }}>${property.newPrice * adults}</Text>
+                    </View>
+
+                    <View style={{ marginTop: 6 }}>
+                        <Text style={{ fontSize: 16, color: "gray" }}>Deluxe Room</Text>
+                        <Text style={{ fontSize: 16, color: "gray" }}>Hotel room: 1 bed</Text>
+                    </View>
+
+                    <View style={{ backgroundColor: "#6082b6", paddingVertical: 2, borderRadius: 5, width: 150, paddingHorizontal: 3, marginTop: 2 }}>
+                        <Text style={{ textAlign: "center", color: "white", }}>Limited time deal</Text>
+                    </View>
+                </View>
+            </Pressable>
         </View>
     )
 }

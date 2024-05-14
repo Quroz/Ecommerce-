8e5818a35d694cf0cbbc04b482c9d748637a0e59
Quroz <filipwt@hotmail.com>
2024-05-14 10:01:15 +0200

    Places Screen

---
+++ components/PropertyCard.jsx
@@ -0,0 +1,14 @@
+import { Pressable, StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const PropertyCard = () => {
+    return (
+        <View>
+            <Pressable />
+        </View>
+    )
+}
+
+export default PropertyCard
+
+const styles = StyleSheet.create({})
